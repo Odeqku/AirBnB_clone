97de90edc4e584062b5693fb642e9e89b0a93f65
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2023-03-13 19:15:02 +0000

    AirBnB Project

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+from engine.file_storage import FileStorage
+
+storage = FileStorage()
+'''Loading the json string back to the dictionary'''
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -6,15 +6,16 @@ uuid: module for generating a unique user id
 datatime: Module for keeping track of time and date"""
 
 import models
+
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """Represents the BaseModel of the AirBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """Initializes a new BaseModel Instance with:
         Args:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
@@ -23,19 +24,25 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if len(kwargs) != 0:
+
+        if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or k == "updated_at":
+                if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.fromisoformat(value)
-                else:
+                elif not key == '__class__':
                     self.__dict__[key] = value
+
+            if not self.__dict__ in FileStorage.__objects:
+                models.storage.new(self)
+
         else:
             pass
 
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
-        
+        '''Save the instance to the file_storage.'''
+        self.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
@@ -52,3 +59,17 @@ class BaseModel:
         """Return the print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new object --")
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model.save()
+print(my_model)
+
